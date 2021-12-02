3f171076875259c420a520810e1be70c8dd0267f
Glitch (at-rps-game) <none>
2021-12-02 01:59:53 +0000

    Part of Week 3

--- js/script.js
+++ js/script.js
@@ -5,14 +5,33 @@ $(".play").click(function() {
   let userInput = $(".input").val();
   $(".userChoice").text(userInput);
   randomNumber = Math.ceil(randomNumber);
-  $(".computerChoice").text(randomNumber);
+  if (randomNumber == 3) {
+    $(".computerChoice").text("Rock");
+  } else if (randomNumber == 2) {
+    $(".computerChoice").text("Paper");
+  } else {
+    $(".computerChoice").text("Scissors");
+  }
 });
 
-// Day 3
-// if (randomNumber == 3) {
-//   $(".computerChoice").text("Rock");
-// } else if (randomNumber == 2) {
-//   $(".computerChoice").text("Paper");
-// } else {
-//   $(".computerChoice").text("Scissors");
-// }
+// $(".play").click(function() {
+//   randomNumber = Math.random() * 3;
+//   let userInput = $(".input").val();
+//   $(".userChoice").text(userInput);
+//   randomNumber = Math.ceil(randomNumber);
+//   if (userInput == ("Rock" || "Paper" || "Scissors") && randomNumber == 3) {
+//     $(".computerChoice").text("Rock");
+//   } else if (
+//     userInput == ("Rock" || "Paper" || "Scissors") &&
+//     randomNumber == 2
+//   ) {
+//     $(".computerChoice").text("Paper");
+//   } else if (
+//     userInput == ("Rock" || "Paper" || "Scissors") &&
+//     randomNumber == 1
+//   ) {
+//     $(".computerChoice").text("Scissors");
+//   } else {
+//     $(".computerChoice").text("That isn't an option I understand!");
+//   }
+// });
