cf0ac13eb2b07ddbd0801e8d7f99a039443021a2
Glitch (at-rps-game) <none>
2021-12-09 01:41:18 +0000

    🏊🏼🕋 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -5,33 +5,14 @@ $(".play").click(function() {
   let userInput = $(".input").val();
   $(".userChoice").text(userInput);
   randomNumber = Math.ceil(randomNumber);
-  if (randomNumber == 3) {
+  if (randomNumber === 3) {
     $(".computerChoice").text("Rock");
-  } else if (randomNumber == 2) {
+    let computerChoice = "Rock";
+  } else if (randomNumber === 2) {
     $(".computerChoice").text("Paper");
+    let computerChoice = "Paper";
   } else {
     $(".computerChoice").text("Scissors");
+    let computerChoice = "Scissors";
   }
 });
-
-// $(".play").click(function() {
-//   randomNumber = Math.random() * 3;
-//   let userInput = $(".input").val();
-//   $(".userChoice").text(userInput);
-//   randomNumber = Math.ceil(randomNumber);
-//   if (userInput == ("Rock" || "Paper" || "Scissors") && randomNumber == 3) {
-//     $(".computerChoice").text("Rock");
-//   } else if (
-//     userInput == ("Rock" || "Paper" || "Scissors") &&
-//     randomNumber == 2
-//   ) {
-//     $(".computerChoice").text("Paper");
-//   } else if (
-//     userInput == ("Rock" || "Paper" || "Scissors") &&
-//     randomNumber == 1
-//   ) {
-//     $(".computerChoice").text("Scissors");
-//   } else {
-//     $(".computerChoice").text("That isn't an option I understand!");
-//   }
-// });
